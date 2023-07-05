5d243e058dcae8c85cc829d7531ad37eccf2d11f
mohammadreza memar <moreza.memar@gmail.com>
2023-07-05 12:57:57 -0500

    tessting the call back function

--- js-Bootcamp/Arrays/notes.js
+++ js-Bootcamp/Arrays/notes.js
@@ -1,5 +1,11 @@
 const notes = ['Note 1', 'Note 2', 'Note 3'];
 
+notes[2] = 'This is the New Note 3';
+
+notes.forEach(function () {
+  console.log('testing 123');
+});
+
 console.log(notes.length);
 
-console.log(notes[0]);
+console.log(notes);
